c7d49e3ce4c0b54d1ecbcd89d6085ddf9cc08048
Michael Mroz <mroz15@hotmail.com>
2022-07-18 21:24:16 -0400

    complete 3.2 - add GET route for info

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const persons = require('./persons')
 
-app.get('/api/persons', (req, res) =>{
+app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const pluralCheck = persons.length === 1 ? 'person' : 'people'
+  const info = `<p>Phonebook has info for ${persons.length} ${pluralCheck} </p>`
+  res.send(info + new Date())
 })
 
 const PORT = 3001
