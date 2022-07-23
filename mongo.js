44bf66826c822a2f2f70edb09e24d2a5f903ee5e
Michael Mroz <mroz15@hotmail.com>
2022-07-23 14:52:52 -0400

    complete 3.12 - add mongoose, mongo.js to write and read from external db

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const pw = process.argv[2]
+
+const url = `mongodb+srv://MisterOz93:${pw}@cluster0.oldzd.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = new mongoose.model('Person', personSchema)
+
+mongoose.connect(url).then(res => {
+  console.log('mongodb connected')
+  if (process.argv.length === 5){
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4]
+    })
+   return person.save().then( res => {
+     console.log(`Added ${person.name} number ${person.number} to phonebook.`)
+     mongoose.connection.close()
+   })
+  }
+  if (process.argv.length === 3){
+    console.log('Phonebook:')
+    Person.find({}).then(res => {
+      res.forEach(person => console.log(`${person.name} ${person.number}`))
+      mongoose.connection.close()
+    })
+  }
+
+})
